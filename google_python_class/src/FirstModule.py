3e5833611c84415c623d991f624592a4c30aba75
Anand Girish B. Parthasarathy <code.ag@outlook.com>
2017-02-26 13:32:50 -0800

    Python learning module from google

---
+++ google_python_class/src/FirstModule.py
@@ -0,0 +1,19 @@
+'''
+Created on Feb 23, 2017
+
+@author: anand.parthasarathy
+'''
+
+if __name__ == '__main__':
+    pass
+
+print 'Hello World of Python'
+
+i = 1
+for i in range(1, 10):
+    if i <= 5 :
+        print 'Smaller or equal then 5.\n',
+    else:
+        print 'Larger then 5.\n',
+        
+print 'Hello' + str(6)
