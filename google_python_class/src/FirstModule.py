3426c86fe5e1068177a5b413fa46dd8577c9fec6
Anand Girish Parthasarathy <anandpartha@fb.com>
2023-04-24 12:53:40 -0700

    Trying to create a new wash sale processor

--- google_python_class/src/FirstModule.py
+++ google_python_class/src/FirstModule.py
@@ -7,13 +7,13 @@ Created on Feb 23, 2017
 if __name__ == '__main__':
     pass
 
-print 'Hello World of Python'
+print('Hello World of Python')
 
 i = 1
 for i in range(1, 10):
     if i <= 5 :
-        print 'Smaller or equal then 5.\n',
+        print('Smaller or equal then 5.\n')
     else:
-        print 'Larger then 5.\n',
-        
-print 'Hello' + str(6)
+        print('Larger then 5.\n')
+
+print('Hello' + str(6))
