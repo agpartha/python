db162f898e56f7c76f74d1e89b33d22b4929ddb7
Anand Girish Parthasarathy <anandpartha@fb.com>
2023-04-26 21:30:52 -0700

    Add defaults for Schwab csv files we use this mainly, user can specify, but an skip by pressing Enter to speed up with only filename

--- wash/main.py
+++ wash/main.py
@@ -8,9 +8,14 @@ import os
 
 # Define a few state variables
 trans_file_name =""
+trans_out_file_name = ""
 skip_rows = []
-header_row = 0
-ticker_column_name = "Symbol"
+skip_rows_def = [0]
+header_row = 1
+ticker_column_name = ""
+ticker_column_name_def = "Symbol"
+tickers = []
+tickers_def = ["FB", "META"]
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -27,37 +32,29 @@ def get_trans_file():
         continue
 
     global header_row
-    while (True):
-        header_row_str = input("Header Row # ? ")
-        if "" != header_row_str:
-            header_row = int(header_row_str)
-            print('Header Row # : ' + str(header_row))
-            break
-        print('Sorry missing Header Row #....')
-        continue
+    header_row_str = input("Header Row # ?, Enter for default (" + str(header_row) + ") ")
+    if "" != header_row_str:
+        header_row = int(header_row_str)
+    print('Using Header Row # : ' + str(header_row))
 
     global ticker_column_name
-    while (True):
-        ticker_column_name = input("Column name for the ticker symbols ?  ")
-        if "" != ticker_column_name:
-            print('Column name for the ticker symbols: ' + ticker_column_name)
-            break
-        print('Sorry missing Column name for the ticker symbols....')
-        continue
+    ticker_column_name = input("Column name for the ticker symbols ?, Enter for default(" + ticker_column_name_def +") ")
+    if "" == ticker_column_name:
+        ticker_column_name = ticker_column_name_def
+    print('Using Column name for the ticker symbols: ' + ticker_column_name)
 
     global skip_rows
-    skip_rows = [int(x) for x in input("Please enter row numbers to skip, separated by space: ").split()]
-    if  len(skip_rows) != 0:
-        print(skip_rows)
+    skip_rows = [int(x) for x in input("Please enter row numbers to skip, separated by space, Enter for default(" + str(skip_rows_def) + ") ").split()]
+    if  len(skip_rows) == 0:
+        skip_rows = skip_rows_def
+    print(skip_rows)
 
 def get_tickers():
     global tickers
-    while (True):
-        tickers = input("Please enter ticket symbols separated by space: ").split()
-        if  len(tickers) != 0:
-            print(tickers)
-            return
-        print('Sorry no tickers to filter, please specify the symbols of equities to filter')
+    tickers = input("Please enter ticket symbols separated by space, Enter for default(" + str(tickers_def) + ") ").split()
+    if  len(tickers) == 0:
+        tickers = tickers_def
+    print("Filtering for symbols " + str(tickers))
 
 def dump_file(filename):
     with open(filename) as csv_file:
@@ -75,6 +72,7 @@ def dump_file(filename):
 def filter_transactions(trans_file_name: str, ticker_column_name: str, header_row: int, skip_rows: list):
     dirname = os.path.dirname(trans_file_name)
     basename, extname = os.path.splitext(os.path.basename(trans_file_name))
+    global trans_out_file_name
     trans_out_file_name = dirname + '/' + basename + '_out' + extname
     print('Transactions output file ', trans_out_file_name)
     with open(trans_file_name) as csv_file:
@@ -100,4 +98,5 @@ if __name__ == '__main__':
     get_trans_file()
     get_tickers()
     filter_transactions(trans_file_name, ticker_column_name, header_row, skip_rows)
+    dump_file(trans_out_file_name)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
